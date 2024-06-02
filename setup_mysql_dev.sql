3668a5238fbeeef4664a15b57c28e05f86b2a73f
Samtegg <137792099+Samtegg@users.noreply.github.com>
2024-06-02 03:48:16 +0100

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,7 @@
--- prepares a MySQL server for the project
+-- This prepares a MySQL server for the project
+
+
+
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
