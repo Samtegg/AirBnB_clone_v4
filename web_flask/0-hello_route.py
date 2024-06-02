725b3de33bc589b317ef2ac6ea7029600cf82fd0
Samtegg <137792099+Samtegg@users.noreply.github.com>
2024-06-02 04:03:43 +0100

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
+""" This starts a Flash Web Application """
 from flask import Flask
 app = Flask(__name__)
+
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ Prints a Message when / is called """
     return 'Hello HBNB!'
+
 
 if __name__ == "__main__":
     """ Main Function """
