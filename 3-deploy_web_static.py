11da468bb9e12ec193742710f08a22c912cf03e5
Samtegg <137792099+Samtegg@users.noreply.github.com>
2024-06-02 03:54:33 +0100

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """
-Fabric script based on the file 2-do_deploy_web_static.py that creates and
+ This is the Fabric script based on the file 2-do_deploy_web_static.py that creates and
 distributes an archive to the web servers
 """
+
+
+
 
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
 env.hosts = ['142.44.167.228', '144.217.246.195']
+
+
 
 
 def do_pack():
@@ -21,6 +26,8 @@ def do_pack():
         return file_name
     except:
         return None
+
+
 
 
 def do_deploy(archive_path):
@@ -42,6 +49,9 @@ def do_deploy(archive_path):
         return True
     except:
         return False
+
+
+
 
 
 def deploy():
